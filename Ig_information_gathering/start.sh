0deaab164609a3900ddb4ae7cd42e7a98e57f8b5
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-07-21 14:59:55 +0530

    Update start.sh

--- Ig_information_gathering/start.sh
+++ Ig_information_gathering/start.sh
@@ -1,17 +1,8 @@
 clear
-echo -e "$Yellow                            processing please wait..>"
+echo -e "$Yellow                            processing please wait ---> 13%"
 sleep 2.0
 clear
-echo -e "$Yellow                            processing please wait...>"
-sleep 2.0
-clear
-echo -e "$Yellow                            processing please wait....>"
-sleep 2.0
-clear
-echo -e "$Yellow                            processing please wait.....>"
-sleep 2.0
-clear
-echo -e "$Yellow                            processing please wait......>"
+echo -e "$Yellow                            processing please wait ----------------> 100%"
 sleep 2.0
 clear
 echo " "
