ffae2f09f1ab576daa7864ef37056b0df9620d52
Samarth Magdum (SM9) <101000624+HackerSM9@users.noreply.github.com>
2022-08-26 17:03:07 +0530

    Update .page.py

--- .page.py
+++ .page.py
@@ -1,4 +1,4 @@
 import os,sys
 
-usr = input("\n\033[1;32mHackerSM9 / UserName $")
-os.system("python3 id_insta -u {0}")
+usr = input("\n\033[1;32mHackerSM9 / UserName $ ")
+os.system("python3 id_insta.py -u {0}")
